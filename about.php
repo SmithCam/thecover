c90730affcca0ef0597d480401e87458972f83f7
Cameron Smith <camwsmith99@gmail.com>
2022-11-20 17:40:03 -0500

    Photo and about page update

--- about.php
+++ about.php
@@ -80,17 +80,6 @@
 								</header>
 
 								<div class ="row">
-								<h2 class ="about-social-label">The Cover:</h2>
-									<ul class="icons">
-										<li><a href="https://twitter.com/CoverBookBlog" target ="_blank" class="icon brands fa-twitter about-socials about-socials-left"><span class="label">Twitter</span></a></li>
-										<li><a href="https://www.facebook.com/" target ="_blank" class="icon brands fa-facebook-f about-socials"><span class="label">Facebook</span></a></li>
-										<li><a href="https://www.instagram.com/thecoverbookblog/" target ="_blank" class="icon brands fa-instagram about-socials"><span class="label">Instagram</span></a></li>
-										<li><a href="https://www.goodreads.com/review/list/127761504-cameron-smith?ref=nav_mybooks&shelf=read#" target ="_blank" class="fa-brands fa-goodreads-g about-socials"><span class="label">Goodreads</span></a></li>
-										<li><a href="contact.php" target ="_blank" class="icon solid fa-envelope about-socials"><span class="label">Email</span></a></li>
-									</ul>
-								</div>
-
-								<div class ="row">
 								<h2 class ="about-social-label-personal about-social-label">Personal:</h2>
 									<ul class="icons">
 										<li><a href="https://twitter.com/Crameyon" target ="_blank" class="icon brands fa-twitter about-socials about-socials-left"><span class="label">Twitter</span></a></li>
